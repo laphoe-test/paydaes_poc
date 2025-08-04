6af408609ddf32c62c4e9f578393d90485919f4a
cheonglaphoe <cl_hoe@yahoo.com>
2025-08-05 05:37:06 +0800

    enable encrypt/decrypt util service and database util service in corehr

--- corehr/src/main/java/com/paydaes/corehr/CorehrApplication.java
+++ corehr/src/main/java/com/paydaes/corehr/CorehrApplication.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EntityScan(basePackages = "com.paydaes.entities.model")
 @EnableJpaRepositories(basePackages = "com.paydaes.entities.repository")
-@SpringBootApplication(scanBasePackages = {"com.paydaes.entities", "com.paydaes.corehr"})
+@SpringBootApplication(scanBasePackages = {"com.paydaes.entities", "com.paydaes.corehr","com.paydaes.utils"})
 public class CorehrApplication {
 
     public static void main(String[] args) {
